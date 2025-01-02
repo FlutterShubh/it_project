df64b915ae562a3280216b3fc576d10151324204
FlutterShubh <shubhamkumarpandey.edugaon@gmail.com>
2025-01-02 18:07:31 +0530

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'dart:ui';
 import 'package:flutter/material.dart';
 
 /// Entrypoint of the application.
@@ -43,7 +42,7 @@ class MyApp extends StatelessWidget {
 }
 
 /// Dock of the reorderable [items].
-class Dock<T> extends StatefulWidget {
+class Dock<T extends Object> extends StatefulWidget {
   const Dock({
     super.key,
     this.items = const [],
@@ -61,58 +60,71 @@ class Dock<T> extends StatefulWidget {
 }
 
 /// State of the [Dock] used to manipulate the [_items].
-class _DockState<T> extends State<Dock<T>> {
+class _DockState<T extends Object> extends State<Dock<T>>
+    with SingleTickerProviderStateMixin {
   /// [T] items being manipulated.
-  late final List<T> _items = widget.items.toList();
+  late List<T> _items = widget.items.toList();
+
+  /// Tracks the currently dragged item.
+  T? _draggingItem;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 78,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
         color: Colors.black12,
       ),
       padding: const EdgeInsets.all(4),
-      child: ReorderableListView(
-        scrollDirection: Axis.horizontal,
-        shrinkWrap: true,
-        proxyDecorator: (child, index, animation) =>
-            proxyDecorator(child, index, animation),
-        onReorder: (oldIndex, newIndex) {
-          setState(() {
-            if (newIndex > oldIndex) newIndex -= 1;
-            final item = _items.removeAt(oldIndex);
-            _items.insert(newIndex, item);
-          });
-        },
-        children: _items
-            .asMap()
-            .entries
-            .map((entry) => Padding(
-                  key: ValueKey(entry.key),
-                  padding: const EdgeInsets.symmetric(horizontal: 2),
-                  child: widget.builder(entry.value),
-                ))
-            .toList(),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: _items.map((item) {
+          return LongPressDraggable<T>(
+            data: item,
+            onDragStarted: () {
+              setState(() {
+                _draggingItem = item;
+              });
+            },
+            onDragEnd: (_) {
+              setState(() {
+                _draggingItem = null;
+              });
+            },
+            feedback: AnimatedBuilder(
+              animation: AlwaysStoppedAnimation<double>(1.0),
+              builder: (context, child) {
+                return Transform.scale(
+                  scale: 1.2, // Slight zoom effect during drag
+                  child: Material(
+                    color: Colors.transparent,
+                    child: widget.builder(item),
+                  ),
+                );
+              },
+            ),
+            childWhenDragging: Opacity(
+              opacity: 0.5,
+              child: widget.builder(item),
+            ),
+            child: DragTarget<T>(
+              onWillAccept: (data) => data != item,
+              onAccept: (data) {
+                setState(() {
+                  final oldIndex = _items.indexOf(data);
+                  final newIndex = _items.indexOf(item);
+
+                  _items.removeAt(oldIndex);
+                  _items.insert(newIndex, data);
+                });
+              },
+              builder: (context, candidateData, rejectedData) {
+                return widget.builder(item);
+              },
+            ),
+          );
+        }).toList(),
       ),
     );
   }
-}
-
-Widget proxyDecorator(Widget child, int index, Animation<double> animation) {
-  return AnimatedBuilder(
-    animation: animation,
-    builder: (BuildContext context, Widget? child) {
-      final double animValue = Curves.easeInOut.transform(animation.value);
-      final double elevation = lerpDouble(0, 6, animValue)!;
-      return Material(
-        elevation: elevation,
-        color: Colors.transparent,
-        shadowColor: Colors.transparent,
-        child: child,
-      );
-    },
-    child: child,
-  );
 }
